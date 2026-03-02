543e6192ffcb3dcf1e1f5fd96ac6fa21791604af
Alix <alix.lauvagne971@gmail.com>
2026-03-02 21:49:55 +0100

    add card competence

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,66 +8,59 @@ export default function Home() {
       {/* HERO */}
       <section
         className="hero hero--overlay text-white"
-        style={{ backgroundImage: `url(${heroImage})` }}
-      >
+        style={{ backgroundImage: `url(${heroImage})` }}>
+
         <div className="container text-center hero__content">
-          <h1 className="display-4 fw-bold">Bonjour, je suis John Doe</h1>
-          <h2 className="mb-4">Développeur web full stack</h2>
-          <button className="btn btn-danger">En savoir plus</button>
+            <h1 className="display-4 fw-bold">Bonjour, je suis John Doe</h1>
+            <h2 className="mb-4">Développeur web full stack</h2>
+            <button className="btn btn-danger">En savoir plus</button>
         </div>
       </section>
 
       {/* A PROPOS + COMPETENCES */}
-      <section className="py-5 bg-light">
-        <div className="container">
-          <div className="card shadow-sm border-0">
-            <div className="card-body p-4 p-md-5">
-              <div className="row g-4">
-                {/* Col gauche */}
-                <div className="col-12 col-lg-6">
-                  <h3 className="section-title">A propos</h3>
+        <section className="py-5 bg-light">
+            <div className="container">
+                <div className="about-wrapper">
+                    <div className="card custom-card border-0">
+                        <div className="card-body p-4 p-md-5">
+                            <div className="row g-4">
 
-                  <img
-                    src={aboutImage}
-                    alt="John Doe au travail"
-                    className="img-fluid rounded mb-3"
-                  />
+                                {/* Col gauche */}
+                                <div className="col-12 col-lg-6">
+                                    <h3 className="section-title">A propos</h3>
 
-                  <p className="text-muted">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Optio, necessitatibus consectetur tempore perferendis
-                    nostrum, ex delectus reiciendis impedit aut iure enim
-                    placeat? Natus, neque at?
-                  </p>
-                  <p className="text-muted">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Optio, necessitatibus consectetur tempore perferendis
-                    nostrum, ex delectus reiciendis impedit aut iure enim
-                    placeat? Natus, neque at?
-                  </p>
-                  <p className="text-muted mb-0">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Optio, necessitatibus consectetur tempore perferendis
-                    nostrum, ex delectus reiciendis impedit aut iure enim
-                    placeat? Natus, neque at?
-                  </p>
+                                    <img src={aboutImage} alt="John Doe au travail" className="img-fluid rounded mb-3"/>
+
+                                    <p className="text-muted">
+                                    Passionné par le développement web, je suis actuellement en formation afin de devenir 
+                                    développeur full stack. Curieux et rigoureux, j’aime comprendre le fonctionnement des 
+                                    technologies modernes et créer des interfaces à la fois performantes et intuitives.
+                                    </p>
+
+                                    <p className="text-muted">
+                                    Je travaille principalement avec HTML, CSS, JavaScript et React. J’accorde une attention 
+                                    particulière à la qualité du code, au respect des standards du W3C ainsi qu’à l’optimisation 
+                                    SEO. Chaque projet est pour moi l’occasion d’améliorer mes compétences et d’explorer de nouvelles 
+                                    solutions techniques.
+                                    </p>
+                                </div>
+
+                                {/* Col droite */}
+                                <div className="col-12 col-lg-6">
+                                        <h3 className="section-title">Mes compétences</h3>
+
+                                        <Skill label="HTML5" value={90} barClass="bg-danger" />
+                                        <Skill label="CSS3" value={80} barClass="bg-info" />
+                                        <Skill label="JAVASCRIPT" value={70} barClass="bg-warning" />
+                                        <Skill label="PHP" value={60} barClass="bg-success" />
+                                        <Skill label="REACT" value={50} barClass="bg-primary" />
+                                </div>
+                            </div>   
+                        </div>
+                    </div>
                 </div>
-
-                {/* Col droite */}
-                <div className="col-12 col-lg-6">
-                  <h3 className="section-title">Mes compétences</h3>
-
-                  <Skill label="HTML5" value={90} barClass="bg-danger" />
-                  <Skill label="CSS3" value={80} barClass="bg-info" />
-                  <Skill label="JAVASCRIPT" value={70} barClass="bg-warning" />
-                  <Skill label="PHP" value={60} barClass="bg-success" />
-                  <Skill label="REACT" value={50} barClass="bg-primary" />
-                </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </section>
+        </section>
     </>
   );
 }
